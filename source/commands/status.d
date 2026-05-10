efe52e91a3900fd5927d66becae5cddc4d83075d
Sahan Fernando <sahan.h.fernando@gmail.com>
2026-05-10 23:50:07 +1000

    Improve bring status subcommand

--- source/commands/status.d
+++ source/commands/status.d
@@ -19,16 +19,29 @@ import bring.worktree;
 void status(Context ctx, string[] args) {
     auto paths = ctx.gitRoot.allHashPaths();
     auto isHashResident = ctx.checkResidence(paths);
+    writefln!"%d files tracked.\n"(paths.length);
+    bool hasChanges;
     foreach (path; paths.byKey) {
+        // FIXME: Rethink this, so that we don't keep stat-ing the same files
+        if (!std.file.exists(path)) {
+            writeln("Deleted: ", path);
+            hasChanges = true;
+            continue;
+        }
         auto hashOnDisk = path.hashFileContents;
         enforce(hashOnDisk.algorithm == HashAlgorithm.sha1);
         auto hashHexAsTracked = paths[path].hashHex;
         if (hashOnDisk.hashHex != hashHexAsTracked) {
             writeln("Modified: ", path);
+            hasChanges = true;
             continue;
         }
         if (!isHashResident[hashHexAsTracked]) {
             writeln("Not pushed: ", path);
+            hasChanges = true;
         }
+    }
+    if (!hasChanges) {
+        writeln("All files present and unmodified.");
     }
 }
